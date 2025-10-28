6db390ba0a8149e6a2757468b95f3f740e019ecf
Shrikant Adhav <shrikantadhav11@gmail.com>
2025-10-28 21:06:49 +0530

    Add config to add css, png, svg file to use in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import "./styles.css";
+
+import IMAGE from "../assets/React.png";
+import IMAGE_SVG from "../assets/React.svg";
+
 export const App = () => {
-    return <h1>HI</h1>
-}
+  return (
+    <>
+      <h1>HI</h1>
+      <img src={IMAGE} alt="React Logo" width={100} height={100} />
+      <img src={IMAGE_SVG} alt="React Logo" width={100} height={100} />
+    </>
+  );
+};
